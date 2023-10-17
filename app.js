9d09bcc02cbc6e01dd8004b3d5dbf0d1e9b47c23
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-10-17 15:49:29 +0530

    Implement register and login to view secrets

---
+++ app.js
@@ -0,0 +1,89 @@
+import express from "express";
+import bodyParser from "body-parser";
+import ejs from "ejs";
+import mongoose from "mongoose";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+const dbURL = "mongodb+srv://lmessi10:lmessi10@cluster0.dvkiufo.mongodb.net/userDB?retryWrites=true&w=majority";
+
+const connectionParams={
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+mongoose.connect(dbURL, connectionParams).then(()=>{
+    console.info("Connected to userDB")
+}).catch((e)=>{
+    console.log("Error:",e);
+})
+
+const userSchema ={
+    email: String,
+    password: String
+}
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req,res){
+    res.render("home");
+});
+
+app.get("/login", function(req,res){
+    res.render("login");
+});
+
+app.get("/register", function(req,res){
+    res.render("register");
+});
+
+app.post("/register",function(req,res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    console.log(req.body.username);
+    console.log(req.body.password);
+
+    newUser.save();
+    console.log(req.body.username + " registered successfuly :)");
+    res.render("secrets");
+});
+
+app.post("/login", async function(req,res){
+    const username= req.body.username;
+    const password= req.body.password;
+
+    const found = await User.findOne({email : username});
+    if(found){
+        if(found.password === password){
+            console.log("Logged in Successfully to Secrets :) ");
+            res.render("secrets");
+        }else{
+            console.log("Password you entered was wrong :( , enter correct password");
+        }
+    }else{
+        console.log("Either of your email or password is incorrect or you may not be a registered user. Register yourself.");
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
